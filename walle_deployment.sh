59fcb514d465678899596eff3bf6f2fb83c1b2da
wuxin <wuxin@kuaiyong.net>
2016-04-29 15:21:39 +0800

    sh

---
+++ walle_deployment.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+projectName='ManageApi'
+projectDir='/var/www/test/'$projectName  #项目路径
+packageDir='/var/www/test/temp/'$projectName    #项目第三方包路径
+packageJson=$packageDir'/package.json'  #json包配置文件
+
+if [ ! -d "$projectDir" ]
+then
+    echo "项目'"$projectDir"'不存在"
+    exit
+fi
+
+if [ ! -d "$packageDir" ]
+then
+    mkdir -p $packageDir
+    if [ $? -ne 0 ]
+    then
+        echo "创建目录'"$packageDir"'失败"
+        exit
+    fi
+fi
+
+cd $projectDir
+trueProDir=`pwd -P`  #软连接指向的真实项目地址
+
+ln -s $projectDir'/package.json' $packageJson
+cd $packageDir
+npm install
+
+ln -s $packageDir'/node_modules' $trueProDir'/node_module'
